e07b9bc219fbd795a031a5042306c466ee2e4fa4
Anderson Ribeiro <eu@andersonribeiro.dev>
2022-02-23 11:21:03 +0000

    Challenge Finished.

---
+++ csharp/multiples3or5/src/multiple3or5/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Type a number:");
+
+var number = 0;
+var sumOfNumbers = 0;
+checked
+{
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+for (int i = number-1; i >= 0; i--)
+{
+   if (i % 3 == 0 || i % 5 == 0) {
+       sumOfNumbers+=i;
+   }
+}
+
+Console.WriteLine($"Sum of numbers wich are multiple of 3 or 5: {sumOfNumbers} ") ;
+
+
+
+
