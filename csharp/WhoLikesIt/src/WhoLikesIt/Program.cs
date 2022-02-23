392453cba98fc5f1e019c9186e1e7b64d5b54a1d
Anderson Ribeiro <eu@andersonribeiro.dev>
2022-02-23 18:49:50 +0000

    Challenge 'WhoLikesIt' finished.'

---
+++ csharp/WhoLikesIt/src/WhoLikesIt/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+string[] names = new string[3];
+string likes = Translate(names);
+Console.WriteLine(likes);
+
+
+string Translate(string[] likes){
+
+     List<(int,string)> translatorTemplates   = new List<(int , string )>{
+        (0,"no one likes this"),
+        (1,"{0} likes this "),
+        (2,"{0} and {1} like this"),
+        (3,"{0}, {1} and {2} like this "),
+        (4,"{0}, {1} and {2} others like this.")
+     };
+
+    int totalLikes =  likes == null ? 0  : likes.Where(x=>!string.IsNullOrEmpty(x)).Count();
+
+    int templateSelector = totalLikes > translatorTemplates.Count-1 ? translatorTemplates.Count-1 : totalLikes;
+    string template = translatorTemplates.Where(x=>x.Item1 >= templateSelector ).FirstOrDefault().Item2;
+    int arrayCapacity = totalLikes > 3 ? 3 : totalLikes;
+    object[] names = likes.AsEnumerable().Select((name,index)=> new {name,index}).Where(x=>x.index < 3).Select(x=>x.name).ToArray();
+    if (totalLikes >3)
+        names[arrayCapacity-1] =  (totalLikes -2).ToString();
+    
+    return string.Format(template, names);
+        
+
+}
+
--- csharp/multiples3or5/src/multiple3or5/Program.cs
+++ csharp/multiples3or5/src/multiple3or5/Program.cs
@@ -1,8 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-
-Console.WriteLine("Type a number:");
+﻿Console.WriteLine("Type a number:");
 
 var number = 0;
 var sumOfNumbers = 0;
@@ -11,16 +7,15 @@ checked
     number = Convert.ToInt32(Console.ReadLine());
 }
 
-
-
-for (int i = number-1; i >= 0; i--)
+for (int i = number - 1; i >= 0; i--)
 {
-   if (i % 3 == 0 || i % 5 == 0) {
-       sumOfNumbers+=i;
-   }
+    if (i % 3 == 0 || i % 5 == 0)
+    {
+        sumOfNumbers += i;
+    }
 }
 
-Console.WriteLine($"Sum of numbers wich are multiple of 3 or 5: {sumOfNumbers} ") ;
+Console.WriteLine($"Sum of numbers wich are multiple of 3 or 5: {sumOfNumbers} ");
 
 
 
