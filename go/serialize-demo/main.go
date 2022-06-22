c70bcadf4f39cb22f48d305149c9fa4ba1382696
Anderson Ribeiro <andribeiro@ext.worten.pt>
2022-06-22 11:55:27 +0100

    decode and encode using gob

--- go/serialize-demo/main.go
+++ go/serialize-demo/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"encoding/json"
 	"fmt"
 )
 
 type Girl struct {
-	Name       string
-	Age        int
-	Gender     string
-	Where      string
-	Is_married bool
+	Name       string `json:"name"`
+	Age        int    `json:"age"`
+	Gender     string `json:"gender"`
+	Where      string `json:"where"`
+	Is_married bool   `json:"is_married"`
 }
 
 func main() {
@@ -19,6 +21,47 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	} else {
+		fmt.Println("**********************")
+		fmt.Println("Encoding using json")
+		fmt.Println("**********************")
 		fmt.Println(string(ret))
+	}
+
+	// deserialization
+	g2 := Girl{}
+	err = json.Unmarshal(ret, &g2)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(g2)
+	fmt.Println(g2.Name, g2.Age)
+
+	//using gob
+	g3 := Girl{"Satori", 16, "F", "Oriental spirit ball", false}
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+
+	if err := enc.Encode(g3); err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		fmt.Println("**********************")
+		fmt.Println("Encoding using gob")
+		fmt.Println("**********************")
+		fmt.Println(buf.String())
+	}
+
+	var g3decoded = Girl{}
+	//decodign using gob
+	dec := gob.NewDecoder(bytes.NewBuffer(buf.Bytes()))
+	err = dec.Decode(&g3decoded)
+	if err == nil {
+		fmt.Println("**********************")
+		fmt.Println("Decoded using gob")
+		fmt.Println("**********************")
+		fmt.Println(g3decoded)
+	} else {
+		fmt.Println(err)
 	}
 }
